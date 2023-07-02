aaab4f12a38b1be695644fe3308bc9638dfb5878
Mohamed Mukhtar <mukxdev@gmail.com>
2023-07-03 01:41:49 +0300

    T&C
    Added the terms and conditions in both English and Arabic

--- components/auth/t-and-c.tsx
+++ components/auth/t-and-c.tsx
@@ -53,40 +53,8 @@ export const TermsAndConditions: FC<ITermsAndConditions> = ({
           </h1>
           <div className="w-full h-[30rem] overflow-y-scroll p-6 border border-gray-300 rounded-2xl">
             <div className="mx-auto prose">
-              <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
-              <p>
-                A terms and conditions agreement outlines the website
-                administrator’s rules regarding user behavior and provides
-                information about the actions the website administrator can and
-                will perform. Essentially, your terms and conditions text is a
-                contract between your website and its users. In the event of a
-                legal dispute, arbitrators will look at it to determine whether
-                each party acted within their rights. Creating the best terms
-                and conditions page possible will protect your business from the
-                following: Abusive users: Terms and Conditions agreements allow
-                you to establish what constitutes appropriate activity on your
-                site or app, empowering you to remove abusive users and content
-                that violates your guidelines. Intellectual property theft:
-                Asserting your claim to the creative assets of your site in your
-                terms and conditions will prevent ownership disputes and
-                copyright infringement. Potential litigation: If a user lodges a
-                legal complaint against your business, showing that they were
-                presented with clear terms and conditions before they used your
-                site will help you immensely in court. In short, terms and
-                conditions give you control over your site and legal enforcement
-                if users try to take advantage of your operations.
-              </p>
-
-              <h3>Is a Terms and Conditions Legally Required on My Website?</h3>
-              <p>
-                Technically, no. You aren’t legally required to have a terms and
-                conditions agreement. However, having terms and conditions for
-                websites is considered standard business practice in the US,
-                Canada, the UK, and just about everywhere else — from South
-                Africa to Australia. If you plan to grow your business or expand
-                your user base, a simple website terms and conditions page will
-                provide your site with an additional layer of legal protection.
-              </p>
+              <h3>{t("title")}</h3>
+              <p>{t("body")}</p>
             </div>
           </div>
           {/* Accepted */}
